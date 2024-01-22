3f1c6c5ca3b5839ebf40a170cf9f022dec85ce7d
Alex Johnson <johnsonav1992@gmail.com>
2024-01-22 05:23:50 -0600

    rip out internals

--- src/entries.tsx
+++ src/entries.tsx
@@ -2,7 +2,6 @@ import { createPages } from 'waku';
 
 import { RootLayout } from './templates/root-layout.js';
 import { HomePage } from './templates/home-page.js';
-import { AboutPage } from './templates/about-page.js';
 
 export default createPages(async ({ createPage, createLayout }) => {
   createLayout({
