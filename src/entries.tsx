02497a33fdda9431a0baa6a1e90219f3cfd9f4cb
Alex Johnson <johnsonav1992@gmail.com>
2024-01-22 06:46:23 -0600

    linting

--- src/entries.tsx
+++ src/entries.tsx
@@ -3,16 +3,19 @@ import { createPages } from 'waku';
 import { RootLayout } from './templates/root-layout.js';
 import { HomePage } from './templates/home-page.js';
 
-export default createPages(async ({ createPage, createLayout }) => {
-  createLayout({
-    render: 'static',
-    path: '/',
-    component: RootLayout,
-  });
+export default createPages( async ( {
+    createPage
+    , createLayout
+} ) => {
+    createLayout( {
+        render: 'static'
+        , path: '/'
+        , component: RootLayout
+    } );
 
-  createPage({
-    render: 'static',
-    path: '/',
-    component: HomePage,
-  });
-});
+    createPage( {
+        render: 'dynamic'
+        , path: '/'
+        , component: HomePage
+    } );
+} );
