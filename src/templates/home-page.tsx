bd4112f2ae91c6913ff2df7556f7374013063d5c
Alex Johnson <johnsonav1992@gmail.com>
2024-01-22 06:28:03 -0600

    eslint + cat pics woot

--- src/templates/home-page.tsx
+++ src/templates/home-page.tsx
@@ -1,8 +1,15 @@
 export const HomePage = async () => {
+    const imgData = await fetch( 'https://api.thecatapi.com/v1/images/search' ).then( res => res.json() );
 
-  return (
-    <div>
-      Waku
-    </div>
-  );
+    const [ catPic ] = imgData;
+
+    return (
+        <div>
+            <img
+                src={ catPic.url }
+                width={ catPic.width }
+                height={ catPic.height }
+            />
+        </div>
+    );
 };
